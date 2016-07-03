f16237dd433f2fcb0b5f3ff1a816aeaf38baf2b2
Anze Slosar <slosar@gmail.com>
2016-07-03 05:41:15 -0400

    Fixed idiotic bug, added SEQUEL QSOs

--- py/findQuasars.py
+++ py/findQuasars.py
@@ -16,12 +16,14 @@ for b in bt1:
     w_qso = w_qso | ((da.BOSS_TARGET1 & 2**b) > 0)
 print "Found ",w_qso.sum(), "BOSS objects."
 
-# find matching masks for eBOSS
+# find matching masks for SEQUELS/eBOSS
 ebt1=[10,11,12,13,14,15,16,17,18] ## these are the qso target mask bits 
 ew_qso = np.zeros(len(da),dtype=bool)
-for b in bt1:
+for b in ebt1:
+    # SEQUELS
+    ew_qso = ew_qso | ((da.EBOSS_TARGET0 & 2**b) > 0)
+    # EBOSS proper
     ew_qso = ew_qso | ((da.EBOSS_TARGET1 & 2**b) > 0)
-    ew_qso = ew_qso | ((da.EBOSS_TARGET2 & 2**b) > 0)
 
 print "Found ",ew_qso.sum(), "eBOSS objects."
 
@@ -30,15 +32,19 @@ print "Found", len(frozenset(da[w_qso].THING_ID)), "unique BOSS objects."
 print "Found", len(frozenset(da[ew_qso].THING_ID)), "unique eBOSS objects."
 print "Found", len(frozenset(da[w_tot].THING_ID)), "unique total objects."
 
-#thing id dictionary
-print "Copying..."
-for tid,mjd,plate,fiber,ra,dec in zip(da[w_tot].THING_ID, da[w_tot].MJD, da[w_tot].PLATE,
-                                      da[w_tot].FIBERID, da[w_tot].RA, da[w_tot].DEC):
-    li.append((tid,(mjd,plate,fiber,ra,dec)))
-print "Making dictionary..."
-tid_dic=dict(li)
 
-print "Dictionary length:", len(ti.keys())
+## here we should probably do some healpix fun and first identify healpix pixels and save a file with meta-data.
+## next start building actual co-adds.
+if False:
+    #thing id dictionary
+    print "Copying..."
+    for tid,mjd,plate,fiber,ra,dec in zip(da[w_tot].THING_ID, da[w_tot].MJD, da[w_tot].PLATE,
+                                          da[w_tot].FIBERID, da[w_tot].RA, da[w_tot].DEC):
+        li.append((tid,(mjd,plate,fiber,ra,dec)))
+    print "Making dictionary..."
+    tid_dic=dict(li)
+
+    print "Dictionary length:", len(ti.keys())
 
 
 
