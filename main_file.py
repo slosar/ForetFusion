a62ef81be8843df87324a596102b5e756e12cdbb
Jose A <jvazquez@bnl.gov>
2016-09-29 10:42:22 -0400

    fix a bug for reading files

--- main_file.py
+++ main_file.py
@@ -15,7 +15,7 @@ that satisfy the bit condition and also
 """
 
 def split_pixel(pixel, Qsos):
-    for i, lpix in enumerate(pixel[:10]):
+    for i, lpix in enumerate(pixel[:1]):
         thingid_repeat = Qsos.pix_uniqueid(lpix)
         if not thingid_repeat: continue
         if Qsos.verbose and i % 5 == 0: print ('#-- pix', i, {lpix: thingid_repeat})
--- run_catalog.py
+++ run_catalog.py
@@ -27,11 +27,11 @@ if rank == 0:
     #df_fits = read_fits(dir_files, 'spAll-v5_10_0.fits', spall_cols)
     Qsos    = Qso_catalog(df_fits, verbose = True)
 
-    Qsos.rep_thid    = 3
+    Qsos.rep_thid    = 4
     Qsos.write_master= True
     Qsos.write_ffits = True
-    Qsos.show_plots  = False
-    Qsos.write_names = False
+    Qsos.show_plots  = True
+    Qsos.write_names = True
     Qsos.write_hist  = True
 
     Qsos.filtering_qsos(condition= Qsos.condition)
