c4d374fed461c63aeaba054a7f18a3885b023066
ja_vazquez <jvazquez@bnl.gov>
2016-11-08 13:37:06 -0500

    skyadder working, found a nasty bug

--- sky/skyadder.py
+++ sky/skyadder.py
@@ -15,12 +15,19 @@ loglamstep=1e-4
 Np=(loglamend-loglam0)/loglamstep
 
 meansky=np.zeros(Np)
-meanw=np.zeros(Np)
 
-Nc=3
-Nit=5
+Nc=2
+Nit=50
 contr=[np.zeros(Np) for i in range(Nc)]
 loglam=loglam0+loglamstep*np.arange(Np)
+
+dotest=True
+loglammean=loglam0+Np/2*loglamstep
+tmean=100*(loglam-loglammean)**2
+tcomp1=np.ones(Np)
+tcomp2=loglam
+tcomp3=np.sin(loglam)
+
 
 for iter in range(Nit):
     for varcount in range(-1,Nc):
@@ -28,60 +35,74 @@ for iter in range(Nit):
         current=np.zeros(Np)
         currentw=np.zeros(Np)
         for ifile,filename in enumerate(filelist[::skip]):
+            print filename
             da=pyfits.open(filename)
             for ext in da[4:]:
                 cloglam=ext.data["loglam"]
                 csky=ext.data["sky"]+ext.data["flux"]
                 civar=ext.data["ivar"]
+                ndx=np.array([int(v) for v in ((cloglam-loglam0)/loglamstep+0.5)])
                 ## values of variables
-                var=[np.cos(ext.header["ALT"]/180.*np.pi),
-                            np.sin(ext.header["AZ"]/180.*np.pi),
-                            np.cos(ext.header["AZ"]/180.*np.pi)]
+                var=[np.cos(ext.header["ALT"]/180.*np.pi)-0.5,
+                    np.sin(ext.header["AZ"]/180.*np.pi),
+                    np.cos(ext.header["AZ"]/180.*np.pi)]
+                var=var[:Nc]
+                
                 assert(len(var)==Nc)
+                if dotest:
+                    csky=tmean[ndx]+tcomp1[ndx]*var[0]+tcomp2[ndx]*var[1]#+tcomp3[ndx]*var[2]
                 if (varcount==-1):
                     unwanted=np.zeros(Np)
                     for i in range(Nc):
                         unwanted+=contr[i]*var[i]
                 else:
-                    unwanted=meansky
+                    unwanted=meansky*1.0
                     for i in range(Nc):
                         if (i!=varcount):
                             unwanted+=contr[i]*var[i]
 
-            ndx=np.array([int(v) for v in ((cloglam-loglam0)/loglamstep+0.5)])
-            tmp = 1 if varcount == -1 else var[varcount]
-            current[ndx]+=civar*tmp*(csky-unwanted[ndx])
-            currentw[ndx]+=civar*tmp**2
-            nsp+=1
-    print(current, currentw)
-    current/=currentw
-    if False:
-        plt.plot(current)
-        plt.ylim([0, 50])
-        plt.show()
-	
-        meanw=currentw
+                tmp = 1 if varcount == -1 else var[varcount]
+                current[ndx]+=civar*tmp*(csky-unwanted[ndx])
+                currentw[ndx]+=civar*tmp**2
+                nsp+=1
+        print(current, currentw)
+        current/=currentw
+        current[np.where(current>10)]=10.0
+        current[np.where(current<0)]=0.0
+        
         if (varcount==-1):
-            meansky=current
-        else:
-            contr[varcount]=current
-    if False:
-        plt.plot(current)
-        plt.ylim([-50, 50])
-        plt.show()
-        print "Finished iteration/varcount",iter,varcount
 
-    f=open("meansky%i.txt"%(iter),"w")
-    for l,w,v in zip(loglam,meanw,meansky):
-        if w>0:
-            f.write("%g %g %g \n"%(l,w,v))
-    f.close()
-    for i in range(Nc):
-        f=open("componet%i_%i.txt"%(i,iter),"w")
-        for l,w,v in zip(loglam,meanw,contr[i]):
-            if w>0:
-                f.write("%g %g %g \n"%(l,w,v))
-        f.close()
+            meansky=current*1.0
+
+            f=open("meansky%i.txt"%(iter),"w")
+            for l,w,v in zip(loglam,currentw,meansky):
+                print l,w,v
+                if w>0:
+                    f.write("%g %g %g %g \n"%(l,w,v,100*(l-loglammean)**2))
+            f.close()
+
+        else:
+            contr[varcount]=current*1.0
+            f=open("componet%i_%i.txt"%(varcount,iter),"w")
+            for l,w,v in zip(loglam,currentw,current):
+                if w>0:
+                    if (varcount==0):
+                        tr=1
+                    elif varcount==1:
+                        tr=l
+                    elif varcount==2:
+                        tr=np.sin(l)
+                    f.write("%g %g %g %g  \n"%(l,w,v,tr))
+            f.close()
+
+        if False:
+            plt.plot(current)
+            plt.ylim([0, 50])
+            plt.show()
+            plt.plot(current)
+            plt.ylim([-50, 50])
+            plt.show()
+    print "Finished iteration/varcount",iter,varcount
         
     
 
